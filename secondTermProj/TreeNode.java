9268cfc3058b84146b85a4da90c64f2808837ff7
Samuel Zhang <samuelzhang3@gmail.com>
2015-05-08 10:07:59 -0400

    Node for Wordtree

---
+++ secondTermProj/TreeNode.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class TreeNode {
+  private TreeNode[] children;
+  private char data;
+
+  public TreeNode(char d) {
+    data = d;
+    children = new TreeNode[27]; //One for each letter, 27 for end of word '-'
+  }
+
+  public String toString() {
+    return "" + data;  
+  }
+
+  public char setData(int i, char c) {
+    char temp = children[i];
+    children[i] = c;
+    return temp;
+  }
+
+  public char getData() {
+    return data;
+  }
+
+  public TreeNode getChild(int i) {
+    return children[i];
+  }
+
+  public TreeNode setChild(int i, TreeNode t) {
+    TreeNode temp = children[i];
+    children[i] = t;
+    return temp;
+  }
+}
