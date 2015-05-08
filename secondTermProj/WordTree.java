e6f723afa06d47a1c20c89e1fff2e5c9e6c7e6af
Samuel Zhang <samuelzhang3@gmail.com>
2015-05-08 10:16:50 -0400

    Unfinished structure

---
+++ secondTermProj/WordTree.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class WordTree {
+  private TreeNode root;
+  
+  public WordTree(char r) {
+    root = new TreeNode(r);
+  }
+
+  public void addLetter(char c) {
+    
+  }
+}
