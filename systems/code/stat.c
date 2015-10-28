2d479b899334c8c4f2715b0d3cfc002bcccdb0b3
Samuel Zhang <samuelzhang3@gmail.com>
2015-10-28 14:06:44 -0400

    more

--- systems/code/stat.c
+++ systems/code/stat.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #include <sys/stat.h>
+#include <time.h>
 
 int main() {
 
@@ -10,7 +11,7 @@ int main() {
   stat( "test/randdump", &sb1 );
   // stat ( "test", &sb2 );
 
-  printf("size: %lu\tuser: %d\tmode: %o\tmtime: %lu\n", sb1.st_size, sb1.st_uid, sb1.st_mode, sb1.st_mtime );
+  printf("size: %lu\tuser: %d\tmode: %o\tmtime: %s\n", sb1.st_size, sb1.st_uid, sb1.st_mode, ctime( & (sb1.st_mtime) ));
 
   //printf("size: %lu\tuser: %d\tmode: %o\tmtime: %lu\n", sb2.st_size, sb2.st_uid, sb2.st_mode, sb2.st_mtime );
 
